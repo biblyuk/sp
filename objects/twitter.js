1828f1d449f04225c90771635db1a13338522c63
Luke Blaney <github@lukeblaney.co.uk>
2012-05-23 16:29:23 +0100

    twitter streaming

--- objects/twitter.js
+++ objects/twitter.js
@@ -1,6 +1,34 @@
+var ntwitter = require('ntwitter');
+var openCalais = require ('openCalais');
 
-	exports.startStream = function startStream() {
-		console.log("TODO: start a stream from twitter");
-
-	}
+var twit = new ntwitter(keys);
+var stream;
+exports.startStream = function startStream() {
+	console.log("start a stream from twitter");
+    twit.stream('user', function(stream) {
+      stream.on('data', function (tweet) {
+        console.log(data);
+        openCalais.send({
+       		provider: 'twitter',
+        	message: tweet.text,
+        	timestamp: new Date(tweet.created_at).getTime(),
+        	user: {
+        		realname: tweet.user.name,
+        		username: tweet.user.screen_name,
+        	}
+        })
+      });
+      stream.on('end', function (response) {
+        console.log("Twitter stream disconnected")
+      });
+      stream.on('destroy', function (response) {
+      	console.log("Twitter stream destroyed")
+        // Handle a 'silent' disconnection from Twitter, no end/error event fired
+      });
+    });
+}
+exports.stopStream = function stopStream() {
+	if (!stream) return;
+	stream.destroy();
+}
 
