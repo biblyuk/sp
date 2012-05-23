1bfdf89d0f7252ef12610d57791952acde82d6a0
Matthew Andrews <matthew@andrews.eu.com>
2012-05-23 16:07:43 +0100

    Further work on the realtime UI update api

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@ var realtime = require('../helpers/realtime.js');
 /*
  * GET home page.
  */
-
 exports.index = function(req, res){
 	res.render('index', { title: 'Shortest Path' });
 };
@@ -11,8 +10,7 @@ exports.index = function(req, res){
 /*
  * GET test trigger for IO update
  */
-
 exports.test = function(req, res) {
-	realtime.broadcast('this message is going out to all users');
+	realtime.broadcastSocial('this message is going out to all users');
 	res.send('Done');
 };
