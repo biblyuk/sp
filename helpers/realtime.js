7fb680aa6d8b8cfd9647c2f2040f0a5028c1aa40
Wilson Page <wilsonpage@me.com>
2012-05-23 15:26:20 +0100

    - Initiated socket connection

--- helpers/realtime.js
+++ helpers/realtime.js
@@ -1,5 +1,18 @@
+var
+
+SocketIO = require('socket.io'),
+io;
 
 
-exports.init = function() {
 
+exports.init = function(app) {
+
+	io = SocketIO.listen(app);
+
+console.log('init');
+	io.sockets.on('connection', function (socket) {
+
+		console.log('connection', socket);
+
+	});
 };
