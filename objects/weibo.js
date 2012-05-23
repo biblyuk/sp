6cef74f7d87d69ed25123c7ec133fab6cd02cac5
Matthew Andrews <matthew@andrews.eu.com>
2012-05-23 18:13:22 +0100

    Work on Weibo

--- objects/weibo.js
+++ objects/weibo.js
@@ -1,3 +1,22 @@
+var 
+
+openCalais = require ('./opencalais.js'),
+apiKeys = require('./apikeys.js'),
+stream;
+
 exports.startStream = function startStream() {
+
+	var tapi = weibo.tapi;
+	var appkey = 2669146070, secret = apiKeys.weibo;
+	var oauth_callback_url = 'http://localhost:3000/testingweibo' || 'oob';
+	tapi.init('tsina', appkey, secret, oauth_callback_url);
+	tapi.public_timeline({}, function(error, data, xhr) {
+	    if(error) {
+	        console.error(error);
+	    } else {
+	        console.log(data);
+	    }
+	});
+
 	
 };
